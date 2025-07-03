6274c5859470a73bec5052fec1f091ecda22aef9
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-07-03 14:44:20 -0500

    try external changes

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ['./src/index.ts', './src/pure.ts'],
-  format: ['esm'],
-  dts: true,
-  platform: "browser",
-  external: [
-    '@vitest/browser/context',
-    '@vitest/browser/utils',
-    'vitest'
-  ],
+	entry: ["./src/index.ts", "./src/pure.ts"],
+	format: ["esm"],
+	dts: true,
+	platform: "browser",
+	external: ["@vitest/browser/utils", "vitest"],
 });
