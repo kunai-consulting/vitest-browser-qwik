4af160fb6e5ce026d78ecae2938a80f045387dd5
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-07-03 15:15:23 -0500

    try tsup

--- tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsdown";
-
-export default defineConfig({
-	entry: ["./src/index.ts", "./src/pure.ts"],
-	format: ["esm"],
-	dts: true,
-	platform: "browser",
-	external: ["@vitest/browser/utils", "vitest"],
-});
