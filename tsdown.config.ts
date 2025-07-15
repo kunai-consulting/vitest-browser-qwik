930a68a55c4bbd5147cad851b7a4d2858019ea02
ross-kunaico <ross.acheson@kunaico.com>
2025-07-15 12:56:16 -0600

    Remove explicit external configurations that are covered by peer deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,11 +5,4 @@ export default defineConfig({
 	format: ["esm"],
 	dts: true,
 	platform: "browser",
-	external: [
-		"@builder.io/qwik",
-		"@builder.io/qwik/server",
-		"@vitest/browser/context", 
-		"@vitest/browser/utils", 
-		"vitest"
-	],
 });
