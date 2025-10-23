4577f38f9fc70ba1d05351714f1f935e20681e18
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-10-22 22:14:14 -0500

    fix: ssr plugin exports

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-	entry: ["./src/index.ts", "./src/pure.tsx", "./src/ssr-plugin.ts"],
-	format: ["esm"],
-	dts: true,
-	platform: "browser",
-});
+export default defineConfig([
+	{
+		entry: ["./src/index.ts", "./src/pure.tsx"],
+		format: ["esm"],
+		dts: true,
+		platform: "browser",
+	},
+	{
+		entry: ["./src/ssr-plugin.ts"],
+		format: ["esm"],
+		dts: true,
+		platform: "neutral",
+		external: [
+			/^node:/,
+			"oxc-parser",
+			"oxc-resolver",
+			"magic-string",
+			"@qwik.dev/core",
+			"@qwik.dev/core/optimizer",
+			"@qwik.dev/core/server",
+			"vitest/config",
+			"vitest/node",
+		],
+	},
+]);
